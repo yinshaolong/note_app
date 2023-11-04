df9e6453bd5ba461d091d4c148f8df30f346c249
omgitsjohn <syin18@my.bcit.ca>
2023-11-04 13:33:39 -0700

    moved templates into base since base/views unable to see the html files

--- base/views.py
+++ base/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+
 def home(request):
-    # return render(request, 'home.html')
-    return HttpResponse("testing")
+    return render(request, 'home.html') 
+    return HttpResponse("hello in home")
+def room(request):
+    return render(request, 'room.html')
+    return HttpResponse("hello")
